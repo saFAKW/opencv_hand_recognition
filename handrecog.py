b4dd6eeb3d55d4b16421c2e16af1e550e048a95b
saFAKW <safakw13@gmail.com>
2025-11-05 22:39:15 +0000

    Code detects when each finger is raised or not

--- handrecog.py
+++ handrecog.py
@@ -1,30 +1,30 @@
-#imports
+# imports
 import cv2
 import mediapipe as mp
 
-#video capturing
+# video capturing
 vid = cv2.VideoCapture(0) # access to internal webcam, 1,2,3 for external webcams
 vid.set(3,1280) #sets the range of the window size
 
-#mediapipe paths
+# mediapipe paths
 mphands = mp.solutions.hands
 
-#library 'mediapipe/mphands' pipelining 
+# library 'mediapipe/mphands' pipelining 
 Hands = mphands.Hands()
 Hands = mphands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.7, min_tracking_confidence=0.6)
 
-#library 'mediapipe/mpdraw' pipelining 
+# library 'mediapipe/mpdraw' pipelining 
 mpdraw = mp.solutions.drawing_utils
 
-#main loop
+# main loop
 while True:
     ret, frame = vid.read()
-    #convert from BGR to RGB
-    RGBframe = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) #mediapipe and cv work on different colour spectrums
+    # convert from BGR to RGB
+    RGBframe = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # mediapipe and cv work on different colour spectrums
     result = Hands.process(RGBframe)
 
-    if result.multi_hand_landmarks: #branch for hand detection
-        for handLm in result.multi_hand_landmarks: #handLm 
+    if result.multi_hand_landmarks: # branch for hand detection
+        for handLm in result.multi_hand_landmarks: # handLm is each hand seen by camera, max. 2
             mpdraw.draw_landmarks(frame, handLm, mphands.HAND_CONNECTIONS,
                                   mpdraw.DrawingSpec(color=(0, 0, 255), circle_radius=7,
                                                      thickness=cv2.FILLED),
@@ -33,14 +33,15 @@ while True:
             for id, lm in enumerate(handLm.landmark):
                 h, w, _ = frame.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                #print(id, cx, cy) print to check all is detected
+                print(id, cx, cy)
 
-                if id == 4 :
-                    Tx, Ty = cx, cy
-                    cv2.circle(frame, (Tx, Ty), 6, (255, 0, 0), cv2.FILLED)
-                if id == 8 :
-                    cv2.circle(frame, (cx, cy), 6, (255, 0, 0), cv2.FILLED)
-                    cv2.line(frame, (cx, cy), (Tx, Ty), (255, 0, 0), 5 ) 
+                #check for 1 on finger
+                if id == 12 or id == 16 or id == 20:
+                    barrier = (handLm.landmark[9].y)*h
+                    if cy < barrier: # finger down check
+                        cv2.circle(frame, (cx, cy), 6, (255, 0, 0), cv2.FILLED)
+                    # cv2.line(frame, (cx, cy), (Tx, Ty), (255, 0, 0), 5 )
+
     cv2.imshow("Hand Recognition", frame) # intialises window and default framerate
 
 
