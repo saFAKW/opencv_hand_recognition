244d26f00d8d74a666305ecc7adb0318ea96ef79
saFAKW <safakw13@gmail.com>
2025-11-05 22:08:35 +0000

    hand detection/tracking working

--- handrecog.py
+++ handrecog.py
@@ -4,23 +4,44 @@ import mediapipe as mp
 
 #video capturing
 vid = cv2.VideoCapture(0) # access to internal webcam, 1,2,3 for external webcams
-vid.set(3,960) #sets the range of the window size
+vid.set(3,1280) #sets the range of the window size
 
 #mediapipe paths
 mphands = mp.solutions.hands
 
-#library 'mphands' pipelining 
+#library 'mediapipe/mphands' pipelining 
 Hands = mphands.Hands()
 Hands = mphands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.7, min_tracking_confidence=0.6)
+
+#library 'mediapipe/mpdraw' pipelining 
+mpdraw = mp.solutions.drawing_utils
 
 #main loop
 while True:
     ret, frame = vid.read()
     #convert from BGR to RGB
-    RGBframe = cv2.cvtColor(frame, cv2.COLOUR_BGR2RGB) #mediapipe and cv work on different colour spectrums
+    RGBframe = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) #mediapipe and cv work on different colour spectrums
+    result = Hands.process(RGBframe)
 
+    if result.multi_hand_landmarks: #branch for hand detection
+        for handLm in result.multi_hand_landmarks: #handLm 
+            mpdraw.draw_landmarks(frame, handLm, mphands.HAND_CONNECTIONS,
+                                  mpdraw.DrawingSpec(color=(0, 0, 255), circle_radius=7,
+                                                     thickness=cv2.FILLED),
+                                  mpdraw.DrawingSpec(color=(0, 255, 0), thickness=7)
+                                  )
+            for id, lm in enumerate(handLm.landmark):
+                h, w, _ = frame.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                #print(id, cx, cy) print to check all is detected
 
-    cv2.imshow("Hand Recognition", frame)
+                if id == 4 :
+                    Tx, Ty = cx, cy
+                    cv2.circle(frame, (Tx, Ty), 6, (255, 0, 0), cv2.FILLED)
+                if id == 8 :
+                    cv2.circle(frame, (cx, cy), 6, (255, 0, 0), cv2.FILLED)
+                    cv2.line(frame, (cx, cy), (Tx, Ty), (255, 0, 0), 5 ) 
+    cv2.imshow("Hand Recognition", frame) # intialises window and default framerate
 
 
     if cv2.waitKey(1) == 27:  # ESC to quit
