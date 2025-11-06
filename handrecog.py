208be11921820d6e2a03bc4c8d9c4b5203fecac1
saFAKW <safakw13@gmail.com>
2025-11-06 10:08:24 +0000

    Attempted using a dictionary for monitoring. Failed.

--- handrecog.py
+++ handrecog.py
@@ -16,6 +16,13 @@ Hands = mphands.Hands(static_image_mode=False, max_num_hands=2, min_detection_co
 # library 'mediapipe/mpdraw' pipelining 
 mpdraw = mp.solutions.drawing_utils
 
+#lists
+monitor = {
+    12: 0,
+    16: 0,
+    20: 0
+}
+
 # main loop
 while True:
     ret, frame = vid.read()
@@ -33,13 +40,18 @@ while True:
             for id, lm in enumerate(handLm.landmark):
                 h, w, _ = frame.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                print(id, cx, cy)
-
+                #print(id, cx, cy)
+                count = 0
                 #check for 1 on finger
-                if id == 12 or id == 16 or id == 20:
+                if id == 12 or id == 16 or id == 20 or id == 4:
                     barrier = (handLm.landmark[9].y)*h
+
                     if cy < barrier: # finger down check
-                        cv2.circle(frame, (cx, cy), 6, (255, 0, 0), cv2.FILLED)
+                        cv2.circle(frame, (cx, cy), 6, (0, 225, 0), cv2.FILLED) 
+                        monitor[id] = cy # only if less than 9 then the spot is filled
+                        count += 1
+                        print(count)
+
                     # cv2.line(frame, (cx, cy), (Tx, Ty), (255, 0, 0), 5 )
 
     cv2.imshow("Hand Recognition", frame) # intialises window and default framerate
